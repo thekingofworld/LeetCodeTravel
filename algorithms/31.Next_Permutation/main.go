daada3368c413f2b0287fe40fc58adaf6c278eaf
thekingofworld <904852632@qq.com>
2018-11-07 00:15:40 +0800

    解决leetcode 31

--- algorithms/31.Next_Permutation/main.go
+++ algorithms/31.Next_Permutation/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 /**
 Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
 
@@ -15,9 +20,29 @@ Here are some examples. Inputs are in the left-hand column and its corresponding
  */
 
 func main() {
-
+	nums := []int{4,2,0,2,3,2,0}
+	nextPermutation(nums)
+	fmt.Println(nums)
 }
 
 func nextPermutation(nums []int)  {
-
+	start := -1
+	end := len(nums)
+	for i:=len(nums)-1; i>0; i-- {
+		for j:=i-1; j>=0; j-- {
+			if nums[i] > nums[j] && j > start {
+				start = j
+				end = i
+				break
+			}
+		}
+	}
+	if start != -1 {
+		nums[start],nums[end] = nums[end],nums[start]
+		sort.Ints(nums[start+1:])
+	} else {
+		for i:=0; i<len(nums)/2; i++ {
+			nums[i],nums[len(nums)-1-i] = nums[len(nums)-1-i], nums[i]
+		}
+	}
 }
