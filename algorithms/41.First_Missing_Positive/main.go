26809b7c86743702f286e91d4be5d1d8a6f3eafd
thekingofworld <904852632@qq.com>
2018-12-03 23:00:46 +0800

    解决leetcode 41

--- algorithms/41.First_Missing_Positive/main.go
+++ algorithms/41.First_Missing_Positive/main.go
@@ -2,21 +2,28 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func main() {
-	nums := []int{1, 2, 0}
+	nums := []int{-1,4,2,1,9,10}
 	fmt.Println(firstMissingPositive(nums))
 }
 
 func firstMissingPositive(nums []int) int {
-	sort.Ints(nums)
-	curPositive := 1
-	for _, num := range nums {
-		if num == curPositive {
-			curPositive++
+	n := len(nums)
+	for i:=0; i<n; i++ {
+		if nums[i]-1 >= 0 && nums[i]-1 < n && nums[i] != nums[nums[i]-1] {
+			nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
+			i--
 		}
 	}
-	return curPositive
+
+	nextPositive := 1
+	for i:=0; i<n; i++ {
+		if nums[i] == nextPositive {
+			nextPositive++
+		}
+	}
+
+	return nextPositive
 }
