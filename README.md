d8627a74c61f097a3ddc1d4344dc956df1ccdd20
thekingofworld <904852632@qq.com>
2019-01-25 10:58:51 +0000

    leetcode 85

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ recording the solution of LeetCode problems
 
 | # | Title | Solution | Difficulty |
 |---| ----- | -------- | ---------- |
+|85|[Maximal Rectangle](https://leetcode.com/problems/maximal-rectangle/description/) | [Go](./algorithms/85.Maximal_Rectangle/main.go)|Hard|
 |72|[Edit Distance](https://leetcode.com/problems/edit-distance/description/) | [Go](./algorithms/72.Edit_Distance/main.go)|Hard|
 |70|[Climbing Stairs](https://leetcode.com/problems/climbing-stairs/description/) | [Go](./algorithms/70.Climbing_Stairs/main.go)|Easy|
 |64|[Minimum Path Sum](https://leetcode.com/problems/minimum-path-sum/description/) | [Go](./algorithms/64.Minimum_Path_Sum/main.go)|Medium|
---
+++ algorithms/85.Maximal_Rectangle/main.go
@@ -0,0 +1,80 @@
+package main
+
+import "fmt"
+
+func main() {
+	matrix := [][]byte{
+		{'0','1','1','0','0','1','0','1','0','1'},
+		{'0','0','1','0','1','0','1','0','1','0'},
+		{'1','0','0','0','0','1','0','1','1','0'},
+		{'0','1','1','1','1','1','1','0','1','0'},
+		{'0','0','1','1','1','1','1','1','1','0'},
+		{'1','1','0','1','0','1','1','1','1','0'},
+		{'0','0','0','1','1','0','0','0','1','0'},
+		{'1','1','0','1','1','0','0','1','1','1'},
+		{'0','1','0','1','1','0','1','0','1','1'},
+	}
+	fmt.Println(maximalRectangle(matrix))
+}
+
+func maximalRectangle(matrix [][]byte) int {
+	maxarea := 0
+	m := len(matrix)
+	if m == 0 {
+		return 0
+	}
+	n := len(matrix[len(matrix)-1])
+	if n == 0 {
+		return 0
+	}
+	mem := make([][]int, m)
+	for i:=0; i<m; i++ {
+		mem[i] = make([]int, n)
+	}
+	for i:=0; i<m; i++ {
+		for j:=0; j<n; j++ {
+			if matrix[i][j] == '0' {
+				mem[i][j] = 0
+			} else {
+				if i-1 < 0 {
+					mem[i][j] = 1
+				} else {
+					mem[i][j] = mem[i-1][j] + 1
+				}
+			}
+		}
+	}
+	for i:=0; i<m; i++ {
+		left := make([]int, n)
+		right := make([]int, n)
+		for j:=0; j<n; j++ {
+			if j-1<0 {
+				left[j] = 0
+				continue
+			}
+			if mem[i][j] != 0 && mem[i][j] <= mem[i][j-1] {
+				left[j] = left[j-1] + 1
+			} else {
+				left[j] = 0
+			}
+		}
+		for j:=n-1; j>=0; j-- {
+			if j == (n-1) {
+				right[j] = 0
+				continue
+			}
+			if mem[i][j] != 0 && mem[i][j] <= mem[i][j+1] {
+				right[j] = right[j+1] + 1
+			} else {
+				right[j] = 0
+			}
+		}
+		for j:=0; j<n; j++ {
+			area := mem[i][j] * (left[j] + right[j] + 1)
+			if area > maxarea {
+				maxarea = area
+			}
+		}
+	}
+	return maxarea
+}
