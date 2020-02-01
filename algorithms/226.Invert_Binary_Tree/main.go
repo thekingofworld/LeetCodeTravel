e728a4d3aecd8215478c78f671efd5ee282edd58
thekingofworld <904852632@qq.com>
2020-02-01 20:03:21 +0800

    feat:解决leetcode226

---
+++ algorithms/226.Invert_Binary_Tree/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	root := &TreeNode{
+		Val:   1,
+		Left:  &TreeNode{Val: 2},
+		Right: &TreeNode{Val: 3},
+	}
+	invertTree(root)
+	fmt.Println(root.Left)
+	fmt.Println(root.Right)
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left, root.Right = root.Right, root.Left
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}
